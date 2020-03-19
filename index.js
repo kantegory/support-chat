0b7d514a6953617193aef23c3b7ba876e8faae68
kantegory <davidobryakov@gmail.com>
2020-03-20 00:13:35 +0300

    Add basic messaging functions

--- index.js
+++ index.js
@@ -6,18 +6,31 @@ var io = require('socket.io').listen(server);
 server.listen(3000);
 
 app.get('/', function (request, response) {
-	response.sendFile(__dirname + '/index.html');
+    response.sendFile(__dirname + '/index.html');
 });
 
 users = [];
 connections = [];
 
 io.sockets.on('connection', function (socket) {
-	console.log('Success connection');
-	connections.push(socket);
+    console.log('Success connection');
+    connections.push(socket);
 
-	socket.on('disconnect', function (data) {
-		connections.splice(connections.indexOf(socket), 1);
-		console.log('Disconnect');
-	})
+    socket.on('disconnect', function (data) {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnect');
+    });
+
+    socket.on('sendMessage', function (data) {
+
+        msg = data.msg;
+        user = data.user;
+        users.push(user);
+
+        userId = users.indexOf(user);
+        console.log('New user, user id: ', userId);
+
+
+        io.sockets.emit('addMessage', {msg: msg, user: user, userId: userId});
+    });
 });
