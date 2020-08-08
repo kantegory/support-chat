96deac42b9d5101b416840fea0389b32b61b69a6
kantegory <davidobryakov@gmail.com>
2020-08-08 18:53:56 +0300

    Add two endpoints for showing all active chats for admin

--- index.js
+++ index.js
@@ -12,6 +12,29 @@ app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html')
 })
 
+app.get('/listall', (req, res) => {
+  res.sendFile(__dirname + '/views/listall.html')
+})
+
+app.get('/getall', async (req, res) => {
+  // console.log('this is request', req.query)
+  let msgs = await getMessages(1)
+
+  msgs = msgs.filter(msg => msg.user_id !== 1)
+
+  let chats = [];
+
+  msgs.forEach((msg) => {
+    if (!chats.includes(msg.user_id)) {
+      chats.push(msg.user_id)
+    }
+  })
+
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.write(JSON.stringify({ data: chats }))
+  res.end()
+})
+
 users = {}
 connections = []
 messages = []
